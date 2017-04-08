736baaa9f204ef0f46b8f553269d3c7f1334886c
vkum29 <vkumar76@sapient.com>
2017-04-08 08:18:08 +0530

    Resolved error: given less than two input NaN was returned. Expected was error.

--- app/js/addNumbers.js
+++ app/js/addNumbers.js
@@ -4,4 +4,4 @@
  */
 'use strict';
 
-module.exports = (a, b) => parseInt(a,10) + parseInt(b,10);
+module.exports = (a, b) => a && b ? parseInt(a,10) + parseInt(b,10) : new Error(`invalid input ${a} and ${b}. We expect mandatorily two numbers to be provided.`);
