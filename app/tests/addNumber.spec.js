7bb9a13745f86740c36a483da01913195a03e48a
Vijayendra Kumar <vk.developer.in@gmail.com>
2025-07-27 01:07:59 +0530

    updated packages

--- app/tests/addNumber.spec.js
+++ app/tests/addNumber.spec.js
@@ -3,47 +3,46 @@
  * Use: test adding of two numbers
  */
 
-let add = require('../js/addNumbers');
-let expect = require('chai').expect;
+let add = require("../js/addNumbers");
+let expect = require("chai").expect;
 
 describe("Adding two numbers", function () {
+  it("Adding two number is implemented", function () {
+    expect(add).to.be.not.null;
+    expect(add).to.be.an.instanceOf(Function);
+  });
 
-	it('Adding two number is implemented', function () {
-		expect(add).to.be.not.null;
-		expect(add).to.be.an.instanceOf(Function);
-	});
+  it("Given two numbers (2 and 5), we expect their sum (7) as result", function () {
+    let result = add(2, 5);
+    expect(result).to.be.numeric;
+    expect(result).to.equal(7);
+  });
 
-	it('Given two numbers (2 and 5), we expect their sum (7) as result', function () {
-		let result = add(2, 5);
-		expect(result).to.be.Number;
-		expect(result).to.equal(7);
-	});
+  it('Given numbers as string ("2" and "5"), we expect their sum (7) as result', function () {
+    let result = add("2", "5");
+    expect(result).to.be.numeric;
+    expect(result).to.equal(7);
+  });
 
-	it('Given numbers as string ("2" and "5"), we expect their sum (7) as result', function () {
-		let result = add("2", "5");
-		expect(result).to.be.Number;
-		expect(result).to.equal(7);
-	});
+  it('Given one number and one string (2 and "5"), we expect their sum (7) as result', function () {
+    let result = add(2, "5");
+    expect(result).to.be.numeric;
+    expect(result).to.equal(7);
+  });
 
-	it('Given one number and one string (2 and "5"), we expect their sum (7) as result', function () {
-		let result = add(2, "5");
-		expect(result).to.be.Number;
-		expect(result).to.equal(7);
-	});
+  it('Given invalid input ("alpha" and "charlie"), we expect Error to be thrown', function () {
+    let result = add("alpha", "charlie");
+    expect(result).to.be.throws;
+    expect(result).to.be.not.string;
+  });
 
-	it('Given invalid input ("alpha" and "charlie"), we expect Error to be thrown', function () {
-		let result = add( "alpha", "charlie");
-		expect(result).to.be.Error;
-		expect(result).to.be.not.String;
-	});
+  it("Given only one number (2), we expect exception to be raised", function () {
+    let result = add(2);
+    expect(result).to.be.instanceOf(Error);
+  });
 
-	it('Given only one number (2), we expect exception to be raised', function () {
-		let result = add(2);
-		expect(result).to.be.instanceOf(Error);
-	});
-
-	it('Given no input, we expect exception to be raised', function () {
-		let result = add();
-		expect(result).to.be.instanceOf(Error);
-	});
+  it("Given no input, we expect exception to be raised", function () {
+    let result = add();
+    expect(result).to.be.instanceOf(Error);
+  });
 });
--- readme.md
+++ readme.md
@@ -4,34 +4,35 @@ An attempt to learn and be more familiar to node.js
 
 ## commands
 
-1. npm test - test the app functionality and watch files for changes.
-1. npm start - test app (enabled via npm prestart) and run the appliction if test run was success.
-
+1. pnpm test - test the app functionality and watch files for changes.
+1. pnpm start - test app (enabled via pnpm prestart) and run the application if test run was success.
 
 ## journey:
 
 1. Download and install node
 1. Add node to you env variable "path"
-1. create helloworld.js and write console > helloworld to verify setup
-> git checkout helloworld
-> run node helloworld from terminal
+1. create helloWorld.js and write console > helloWorld to verify setup
+   > git checkout helloWorld
+   > run node helloWorld from terminal
 1. lets setup and create something simple but not trivial
-> A method to add two numbers
+   > A method to add two numbers
 
 ### Please follow: tags as we implement | A method to add two numbers
 
-1. Capturing our requiremtnt: git checkout spec_for_addding_two_numbers
-1. Implement test to verify behaviour: git checkout test_implemented_for_addding_two_numbers
-1. Implement behaviour: git checkout add_two_number_basic_implementation
-1. Fixed few issues with behaviour: git checkout add_two_number_improved_handle_number_given_as_strings
-1. Fixed few more issues with behaviour: git checkout hurray_all_requirement_implemented_for_adding_two_numbers
+1. Capturing our requirement: git checkout spec_for_adding_two_numbers
+1. Implement test to verify behavior: git checkout test_implemented_for_adding_two_numbers
+1. Implement behavior: git checkout add_two_number_basic_implementation
+1. Fixed few issues with behavior: git checkout add_two_number_improved_handle_number_given_as_strings
+1. Fixed few more issues with behavior: git checkout hurray_all_requirement_implemented_for_adding_two_numbers
 
 ### Update: Checkout tag allow_user_to_call_add_by_passing_args_from_command_line
+
 To call add from command line use
-> npm start add num1 num2 [add num1 num2 ...]
+
+> pnpm start add num1 num2 [add num1 num2 ...]
 
 Example:
-$ npm start add 1 2 add 4 5
+$ pnpm start add 1 2 add 4 5
 
 Output:
 Processing your requests. Got 2 add requests
